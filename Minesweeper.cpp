a022985f9e2cd60c2468b2c35585d4c293fd9159
Tanner <43326002+tannerlang@users.noreply.github.com>
2023-11-09 18:01:46 -0500

    Commented out main function for now, add temp object and begin play

--- Minesweeper.cpp
+++ Minesweeper.cpp
@@ -1,30 +1,31 @@
-// Two-Player Minesweeper
+// Two - Player Minesweeper
 #include<SFML/Graphics.hpp>
-#include<iostream>
 #include<time.h> //for time and date functions from C++ standard library
+#include"GameLoop.h"
 
-using namespace sf; //for classes provided by SFML
+//using namespace sf; //for classes provided by SFML
 
 int main()
 {
+	/*
 	srand(time(0));							//RNG function, takes machine time for to generate new random seed.
 
-	RenderWindow MineSweep(VideoMode(400, 400),"Minesweeper");	//initiate window for the game through SFML library.
-	
+	sf::RenderWindow MineSweep(sf::VideoMode(400, 400), "Minesweeper");	//initiate window for the game through SFML library.
+
 	int w = 32;
 	int grid[12][12];
 	int sgrid[12][12]; //initializes size of grid.
 
-	Texture tile;
+	sf::Texture tile;
 	tile.loadFromFile("images/tiles.jpg");	    //load the tiles image from the repo to be used for the grid
-	Sprite Sp(tile);							//SFML library function to draw the texture.
+	sf::Sprite Sp(tile);							//SFML library function to draw the texture.
 
 	//populates sgrid two dimensional array to be then be used by SFML to be displayed.
 	for (int i = 1; i <= 10; i++)
 	{
-		for (int j = 1; j <= 10; j++) 
+		for (int j = 1; j <= 10; j++)
 		{
-			sgrid[i][j] = 10;	
+			sgrid[i][j] = 10;
 
 
 			if (rand() % 5 == 0) //if the random seed is divisible by 5 then set the grid equal to 9
@@ -36,9 +37,9 @@ int main()
 				grid[i][j] = 0;
 			}
 		}
-	}	
+	}
 	//---------------------------------------------------------
-		
+
 
 
 
@@ -50,14 +51,14 @@ int main()
 			int n = 0;
 			if (grid[i][j] == 9) continue;
 			if (grid[i + 1][j] == 9) n++;
-			if (grid[i][j+1] == 9) n++;
-			if (grid[i-1][j] == 9) n++;
-			if (grid[i][j-1] == 9) n++;
+			if (grid[i][j + 1] == 9) n++;
+			if (grid[i - 1][j] == 9) n++;
+			if (grid[i][j - 1] == 9) n++;
 
-			if (grid[i+1][j+1] == 9) n++;
-			if (grid[i-1][j-1] == 9) n++;
+			if (grid[i + 1][j + 1] == 9) n++;
+			if (grid[i - 1][j - 1] == 9) n++;
 			if (grid[i - 1][j + 1] == 9) n++;
-			if (grid[i+1][j-1] == 9) n++;
+			if (grid[i + 1][j - 1] == 9) n++;
 
 			grid[i][j] = n;
 		}
@@ -65,61 +66,65 @@ int main()
 
 	}
 	//------------------------------
-	
+
 
 
 	//------------DISPLAY------------//
-	while (MineSweep.isOpen())
+	while (MineSweep.isOpen())				//gameloop
 	{
-		Vector2i mous = Mouse::getPosition(MineSweep);	//create a vector from sfml lib called mouse, set it equal to the real time mouse
-														//location in the Minesweep window using the sfml getPosition lib function.
+		sf::Vector2i mous = sf::Mouse::getPosition(MineSweep);	//create a vector from sfml lib called mouse, set it equal to the real time mouse
+		//location in the Minesweep window using the sfml getPosition lib function.
 		int x = mous.x / w;
 		int y = mous.y / w;									//initiate x and y vars and set them = to the current coordinates.
 
-		Event ev;
+		sf::Event ev;
 		while (MineSweep.pollEvent(ev))		//checks to see if SFML class Event ev is active, will proceed if true.
 		{
-			if (ev.type == Event::Closed)
+			if (ev.type == sf::Event::Closed)
 			{
 				MineSweep.close();			//closes the MineSweep Window.
 			}
 
-			if (ev.type == Event::MouseButtonPressed)	//using sfml, check to see if a mouse button was pressed.
+			if (ev.type == sf::Event::MouseButtonPressed)	//using sfml, check to see if a mouse button was pressed.
 			{
-				if (ev.key.code == Mouse::Left)
+				if (ev.key.code == sf::Mouse::Left)
 				{
 					sgrid[x][y] = grid[x][y];	//if the left mouse button is clicked, set the sgrid cell spot (blank grid) at... 
-												//the mouse coordinates equal to the populated grid to reveal what is in the cell beneath;
+					//the mouse coordinates equal to the populated grid to reveal what is in the cell beneath;
 				}
-				else if (ev.key.code == Mouse::Right)
+				else if (ev.key.code == sf::Mouse::Right)
 				{
 					sgrid[x][y] = 11;			//if the right button is clicked, set the cell at the mouse coordinates = to 11 (flag);
 				}
 			}
 		}
-		
+
 		//builds the grid to be displayed, using SFML library functions and a nested for loop.
-		MineSweep.clear(Color::Cyan);
+		MineSweep.clear(sf::Color::Cyan);
 		for (int i = 1; i <= 10; i++)
 		{
 			for (int j = 1; j <= 10; j++)
 			{
-				/*
+				
 				if (sgrid[x][y] == 9)
 				{
 					sgrid[i][j] = grid[i][j];		//if u land on a mine;
 				}
-				*/				
+				
 
-				Sp.setTextureRect(IntRect(sgrid[i][j] * w, 0, w, w));
+				Sp.setTextureRect(sf::IntRect(sgrid[i][j] * w, 0, w, w));
 				Sp.setPosition(i * w, j * w);
 				MineSweep.draw(Sp);
 			}
 		}
 		//Display the grid!
 		MineSweep.display();	//SFML function "display()" by calling it with the...
-								//..."MineSweep" object we created using the RenderWindow SFML Class.
+		//..."MineSweep" object we created using the RenderWindow SFML Class.
 	}
 	//-----------------------------------------------------------------------
+	*/
+Game Begin;
+Begin.play();
 
 }
+
